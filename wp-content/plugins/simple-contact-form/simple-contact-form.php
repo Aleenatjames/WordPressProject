a2795191386db8e32bcdc758c9b7346b0f773364
Aleenatjames <aleena.james@yatnam.com>
2024-11-04 18:23:02 +0530

    #5 Plugin development with ajax

--- wp-content/plugins/simple-contact-form/simple-contact-form.php
+++ wp-content/plugins/simple-contact-form/simple-contact-form.php
@@ -18,16 +18,17 @@ class SimpleContactForm
     {
         // Create custom post type
         add_action('init', array($this, 'create_custom_post_type'));
-    
+
         // Add assets (JS, CSS, etc.)
         add_action('wp_enqueue_scripts', array($this, 'load_assets'));
-    
+
         // Add shortcode
         add_shortcode('contact-form', array($this, 'load_shortcode'));
-        
+
         //Register REST API
-        add_action('rest_api_init',array($this,'register_rest_api'));
-  
+        add_action('rest_api_init', array($this, 'register_rest_api'));
+
+        add_action('wp_footer',array($this,'load_scripts'));
     }
 
     public function create_custom_post_type()
@@ -54,7 +55,7 @@ class SimpleContactForm
             'simple-contact-form',
             plugin_dir_url(__FILE__) . 'css/simple-contact-form.css',
             array(),
-            '1.0',
+            '1',
             'all'
         );
 
@@ -62,64 +63,99 @@ class SimpleContactForm
             'simple-contact-form',
             plugin_dir_url(__FILE__) . 'js/simple-contact-form.js',
             array('jquery'), // Ensure jQuery is a dependency
-            '1.0',
-            true // Load in the footer
+            '1',
+            'all'
+           
         );
-
-        // Inline script that depends on jQuery
-        add_action('wp_footer', array($this, 'load_inline_script'));
     }
 
     public function load_shortcode()
     {
-        ob_start(); // Start output buffering to capture HTML
-        ?>
-            <div class="container simple-contact-form">
-                <h1>Send an Email</h1>
-                <p>Please fill out the form below.</p>
-                <form id="simple-contact-form__form">
-                    <div class="form-group mb-2">
-                        <input name="name" type="text" placeholder="Name" class="form-control">
-                    </div>
-                    <div class="form-group mb-2">
-                        <input name="email" type="email" placeholder="Email" class="form-control">
-                    </div>
-                    <div class="form-group mb-2">
-                        <input name="phone" type="tel" placeholder="Phone" class="form-control">
-                    </div>
-                    <div class="form-group mb-2">
-                        <textarea name="message" placeholder="Type your message" class="form-control"></textarea>
-                    </div>
-                    <div class="form-group mb-2">
-                        <button type="submit" class="btn btn-success btn-block">Send Message</button>
-                    </div>
-                </form>
-            </div>
-        <?php
-        return ob_get_clean(); // Return the buffered HTML
+       
+?>
+        <div class="container simple-contact-form">
+            <h1>Send an Email</h1>
+            <p>Please fill out the form below.</p>
+            <form id="simple-contact-form__form">
+                <div class="form-group mb-2">
+                    <input name="name" type="text" placeholder="Name" class="form-control">
+                </div>
+                <div class="form-group mb-2">
+                    <input name="email" type="email" placeholder="Email" class="form-control">
+                </div>
+                <div class="form-group mb-2">
+                    <input name="phone" type="tel" placeholder="Phone" class="form-control">
+                </div>
+                <div class="form-group mb-2">
+                    <textarea name="message" placeholder="Type your message" class="form-control"></textarea>
+                </div>
+                <div class="form-group mb-2">
+                    <button type="submit" class="btn btn-success btn-block">Send Message</button>
+                </div>
+            </form>
+        </div>
+    <?php
+        
     }
 
-    public function load_inline_script()
+    public function load_scripts()
     {
-        ?>
-       
-        <?php
+    ?>
+        <script>
+            var nonce = '<?php echo wp_create_nonce('wp_rest'); ?>';
+
+
+            jQuery(document).ready(function($) {
+                $('#simple-contact-form__form').submit(function(event) {
+                    event.preventDefault();
+                    var form = $(this).serialize();
+
+                    $.ajax({
+                        method: 'post',
+                        url: '<?php echo get_rest_url(null, 'simple-contact-form/v1/send-email'); ?>',
+                        headers: {
+                            'X-WP-Nonce': nonce
+                        },
+                        data: form
+                    })
+                });
+            });
+        </script>
+<?php
     }
-    public function register_rest_api(){
-        register_rest_route('simple-contact-form/v1',
-        'send-email',
-        array(
-            'methods'=>'POST',
-            'callback'=>array($this,'handle_contact_form')
-        )
-    );
+    public function register_rest_api()
+    {
+        register_rest_route(
+            'simple-contact-form/v1',
+            'send-email',
+            array(
+                'methods' => 'POST',
+                'callback' => array($this, 'handle_contact_form')
+            )
+        );
     }
-    public function handle_contact_form($data){
-        echo 'Helo,this endpoint is working';
+    public function handle_contact_form($data)
+    {
+        $headers = $data->get_headers();
+        $params =$data->get_params();
+        $nonce = $headers['x_wp_nonce'][0];
+
+        if(!wp_verify_nonce($nonce,'wp_rest')){
+            return new WP_REST_Response('Message not sent',422);
+            
+        }
+        $post_id = wp_insert_post([
+            'post_type' =>'simple_contact_form',
+            'post_title' =>'Contact Enquiry',
+            'post_status' =>'publish'
+        ]);
+        if($post_id){
+            return new WP_REST_Response('Thank you for your email',200);
+          }
     }
-}   
+}
 
 // Instantiate the class
-new SimpleContactForm();
+new SimpleContactForm;
 
 ?>
