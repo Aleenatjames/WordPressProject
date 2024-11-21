8e4537fbf683d2ac3a986396a842e41f924286a1
Aleenatjames <aleena.james@yatnam.com>
2024-11-21 14:31:23 +0530

    #7 updated logo link and social media icons in footer.php

--- wp-content/themes/Luigi's/footer.php
+++ wp-content/themes/Luigi's/footer.php
@@ -1,9 +1,12 @@
 <footer class="pb-50  pt-70 pos-relative">
         <div class="pos-top triangle-bottom"></div>
         <div class="container-fluid">
-                <a href="index.html"> <?php if (function_exists('the_custom_logo')) {
-                                                the_custom_logo();
-                                        } ?></a>
+                <a href="<?php echo esc_url(home_url('/')); ?>">
+                        <?php if (function_exists('the_custom_logo')) {
+                                the_custom_logo();
+                        } ?>
+                </a>
+
                 <?php
                 $address = get_field('address', 'option');
                 $phone = get_field('phone', 'option');
@@ -11,33 +14,79 @@
                 ?>
 
                 <div class="pt-30">
-                                <p class="underline-secondary"><b><?php echo esc_html__('Address:', 'luigis'); ?></b></p>
+                        <p class="underline-secondary"><b><?php echo esc_html__('Address:', 'luigis'); ?></b></p>
                         <?php if (!empty($address)) : ?>
                                 <p><b><?php echo esc_html($address); ?></b></p>
                         <?php endif; ?>
                 </div>
 
                 <div class="pt-30">
-                                <p class="underline-secondary mb-10"><b><?php echo esc_html__('Phone:', 'luigis'); ?></b></p>
+                        <p class="underline-secondary mb-10"><b><?php echo esc_html__('Phone:', 'luigis'); ?></b></p>
                         <?php if (!empty($phone)) : ?>
                                 <a href="tel:<?php echo esc_attr($phone); ?>"><b><?php echo esc_html($phone); ?></b></a>
                         <?php endif; ?>
                 </div>
 
                 <div class="pt-30">
-                                <p class="underline-secondary mb-10"><b><?php echo esc_html__('Email:', 'luigis'); ?></b></p>
+                        <p class="underline-secondary mb-10"><b><?php echo esc_html__('Email:', 'luigis'); ?></b></p>
                         <?php if (!empty($email)) : ?>
                                 <a href="mailto:<?php echo esc_attr($email); ?>"><b><?php echo esc_html($email); ?></b></a>
                         <?php endif; ?>
                 </div>
 
                 <ul class="icon mt-30">
-                        <li><a href="#"><i class="ion-social-pinterest"></i></a></li>
-                        <li><a href="#"><i class="ion-social-facebook"></i></a></li>
-                        <li><a href="#"><i class="ion-social-twitter"></i></a></li>
-                        <li><a href="#"><i class="ion-social-dribbble-outline"></i></a></li>
-                        <li><a href="#"><i class="ion-social-linkedin"></i></a></li>
-                        <li><a href="#"><i class="ion-social-vimeo"></i></a></li>
+                        <?php
+                        // Retrieve the entire social media links group field
+                        $social_media_links = get_field('social_media_links', 'option');
+                        ?>
+
+                        <?php if (!empty($social_media_links['pinterest_url']['url'])): ?>
+                                <li>
+                                        <a href="<?php echo esc_url($social_media_links['pinterest_url']['url']); ?>">
+                                                <i class="ion-social-pinterest"></i>
+                                        </a>
+                                </li>
+                        <?php endif; ?>
+
+                        <?php if (!empty($social_media_links['facebook_url']['url'])): ?>
+                                <li>
+                                        <a href="<?php echo esc_url($social_media_links['facebook_url']['url']); ?>">
+                                                <i class="ion-social-facebook"></i>
+                                        </a>
+                                </li>
+                        <?php endif; ?>
+
+                        <?php if (!empty($social_media_links['twitter_url']['url'])): ?>
+                                <li>
+                                        <a href="<?php echo esc_url($social_media_links['twitter_url']['url']); ?>">
+                                                <i class="ion-social-twitter"></i>
+                                        </a>
+                                </li>
+                        <?php endif; ?>
+
+                        <?php if (!empty($social_media_links['dribbble_url']['url'])): ?>
+                                <li>
+                                        <a href="<?php echo esc_url($social_media_links['dribbble_url']['url']); ?>">
+                                                <i class="ion-social-dribbble-outline"></i>
+                                        </a>
+                                </li>
+                        <?php endif; ?>
+
+                        <?php if (!empty($social_media_links['linkedin_url']['url'])): ?>
+                                <li>
+                                        <a href="<?php echo esc_url($social_media_links['linkedin_url']['url']); ?>">
+                                                <i class="ion-social-linkedin"></i>
+                                        </a>
+                                </li>
+                        <?php endif; ?>
+
+                        <?php if (!empty($social_media_links['vimeo_url']['url'])): ?>
+                                <li>
+                                        <a href="<?php echo esc_url($social_media_links['vimeo_url']['url']); ?>">
+                                                <i class="ion-social-vimeo"></i>
+                                        </a>
+                                </li>
+                        <?php endif; ?>
                 </ul>
 
                 <p class="color-light font-9 mt-50 mt-sm-30">
