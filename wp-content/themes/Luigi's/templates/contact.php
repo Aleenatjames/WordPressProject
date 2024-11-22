5b56c322ded8fe0af9e11696abb0597da1cee1ee
Aleenatjames <aleena.james@yatnam.com>
2024-11-22 09:43:29 +0530

    #7 updated the contact.php using wp standard theme development

--- wp-content/themes/Luigi's/templates/contact.php
+++ wp-content/themes/Luigi's/templates/contact.php
@@ -3,53 +3,70 @@
 ?>
 
 <?php get_header();
-if ($main['big_title']): ?>
-        <section class="bg-6 h-500x main-slider pos-relative">
-                <div class="triangle-up pos-bottom"></div>
-                <div class="container h-100">
-                        <div class="dplay-tbl">
-                                <div class="dplay-tbl-cell center-text color-white pt-90">
-                                        <?php $main = get_field('main'); ?>
-                                        <?php if ($main['small_title']): ?>
-                                                <h5><b><?php echo $main['small_title']; ?></b></h5>
-                                        <?php endif; ?>
-                                        <h3 class="mt-30 mb-15"><?php echo $main['big_title']; ?></h3>
-                                </div>
-                        </div>
+$main = get_field('main'); 
+if (!empty($main) && !empty($main['big_title'])) : ?>
+    <section class="bg-6 h-500x main-slider pos-relative">
+        <div class="triangle-up pos-bottom"></div>
+        <div class="container h-100">
+            <div class="dplay-tbl">
+                <div class="dplay-tbl-cell center-text color-white pt-90">
+                    <?php if (!empty($main['small_title'])) : ?>
+                        <h5><b><?php echo esc_html($main['small_title']); ?></b></h5>
+                    <?php endif; ?>
+                    <h3 class="mt-30 mb-15"><?php echo esc_html($main['big_title']); ?></h3>
                 </div>
-        </section>
+            </div>
+        </div>
+    </section>
 <?php endif; ?>
 
-<section class="story-area left-text center-sm-text">
-        <div class="container">
-                <div class="heading">
-                        <img class="heading-img" src="<?php bloginfo('template_directory'); ?>/images/heading_logo.png" alt="">
-                        <?php $hello = get_field('hello'); ?>
-                        <?php if ($hello['heading']): ?>
-                                <h2><?php echo $hello['heading']; ?></h2>
-                        <?php endif; ?>
-                        <?php if ($hello['sub_heading']): ?>
-                                <h5 class="mt-10 mb-30"><?php echo $hello['sub_heading']; ?></h5>
-                        <?php endif; ?>
-                        <?php if ($hello['description']): ?>
-                                <p class="mx-w-700x mlr-auto"><?php echo $hello['description']; ?></p>
-                        <?php endif; ?>
-                </div>
 
-                <form class="form-style-1 placeholder-1">
-                        <div class="row">
-                                <div class="col-md-6"> <input class="mb-20" type="text" placeholder="Name"> </div>
-                                <div class="col-md-6"> <input class="mb-20" type="text" placeholder="E-mail"> </div>
-                                <div class="col-md-12"><input class="mb-20" type="text" placeholder="Subject"> </div>
-                                <div class="col-md-12">
-                                        <textarea class="h-200x ptb-20" placeholder="Message"></textarea>
-                                </div>
-                        </div>
-                        <?php if ($hello['link']['url']): ?>
-                                <h6 class="center-text mtb-30"><a href="<?php echo $hello['link']['url'] ?>" class="btn-primaryc plr-25"><b><?php echo $hello['link_text']; ?></b></a></h6>
-                        <?php endif; ?>
-                </form>
+<section class="story-area left-text center-sm-text">
+    <div class="container">
+        <div class="heading">
+            <img class="heading-img" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/heading_logo.png" alt="Heading Logo">
+            
+            <?php 
+            $hello = get_field('hello'); // Retrieve the 'hello' field
+            
+            if (!empty($hello['heading'])) : ?>
+                <h2><?php echo esc_html($hello['heading']); ?></h2>
+            <?php endif; ?>
+
+            <?php if (!empty($hello['sub_heading'])) : ?>
+                <h5 class="mt-10 mb-30"><?php echo esc_html($hello['sub_heading']); ?></h5>
+            <?php endif; ?>
+
+            <?php if (!empty($hello['description'])) : ?>
+                <p class="mx-w-700x mlr-auto"><?php echo esc_html($hello['description']); ?></p>
+            <?php endif; ?>
         </div>
+
+        <form class="form-style-1 placeholder-1">
+            <div class="row">
+                <div class="col-md-6">
+                    <input class="mb-20" type="text" name="name" placeholder="Name">
+                </div>
+                <div class="col-md-6">
+                    <input class="mb-20" type="email" name="email" placeholder="E-mail">
+                </div>
+                <div class="col-md-12">
+                    <input class="mb-20" type="text" name="subject" placeholder="Subject">
+                </div>
+                <div class="col-md-12">
+                    <textarea class="h-200x ptb-20" name="message" placeholder="Message"></textarea>
+                </div>
+            </div>
+            
+            <?php if (!empty($hello['link']['url']) && !empty($hello['link_text'])) : ?>
+                <h6 class="center-text mtb-30">
+                    <a href="<?php echo esc_url($hello['link']['url']); ?>" class="btn-primaryc plr-25">
+                        <b><?php echo esc_html($hello['link_text']); ?></b>
+                    </a>
+                </h6>
+            <?php endif; ?>
+        </form>
+    </div>
 </section>
 
 
